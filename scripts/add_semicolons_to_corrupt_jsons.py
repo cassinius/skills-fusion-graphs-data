72cf9f2698e18f091032573c9590666c18967953
Bernd Malle <bernd.malle@gmail.com>
2020-07-20 10:23:13 +0200

    added quick json stats for testing

--- scripts/add_semicolons_to_corrupt_jsons.py
+++ scripts/add_semicolons_to_corrupt_jsons.py
@@ -1,12 +1,13 @@
+import json
 from pathlib import Path
 
 EXT = '.json'
 INPUT_DIR = Path('data/corrupt_jsons')
 OUTPUT_DIR = Path('data')
-SKILLS_IN = INPUT_DIR / ('cleaned_related_skills' + EXT)
-JOBS_IN = INPUT_DIR / ('cleaned_related_jobs' + EXT)
-SKILLS_OUT = OUTPUT_DIR / ('cleaned_related_skills' + EXT)
-JOBS_OUT = OUTPUT_DIR / ('cleaned_related_jobs' + EXT)
+SKILLS_CORRUPT = INPUT_DIR / ('related_skills' + EXT)
+JOBS_CORRUPT = INPUT_DIR / ('related_jobs' + EXT)
+SKILLS_JSON = OUTPUT_DIR / ('cleaned_related_skills' + EXT)
+JOBS_JSON = OUTPUT_DIR / ('cleaned_related_jobs' + EXT)
 
 
 def insert_semicolons(file):
@@ -20,13 +21,35 @@ def insert_semicolons(file):
   return '\n'.join(line_arr)
 
 
-if __name__ == "__main__":
-  corrupt_skills = open(SKILLS_IN)
+def correct_jsons():
+  corrupt_skills = open(SKILLS_CORRUPT)
   skills = insert_semicolons(corrupt_skills)
-  with open(SKILLS_OUT, 'w') as f:
+  with open(SKILLS_JSON, 'w') as f:
     f.write(skills)
-  corrupt_jobs = open(JOBS_IN)
+  corrupt_jobs = open(JOBS_CORRUPT)
   jobs = insert_semicolons(corrupt_jobs)
-  with open(JOBS_OUT, 'w') as f:
+  with open(JOBS_JSON, 'w') as f:
     f.write(jobs)
-  
+
+
+def print_stats(file):
+  with open(file) as f:
+    min_len = float('inf')
+    max_len = float('-inf')
+    avg_len = 0
+    data = json.load(f)
+    entries = data["entries"]
+    for idx, entry in enumerate(entries):
+      min_len = len(entry) if len(entry) < min_len else min_len
+      max_len = len(entry) if len(entry) > max_len else max_len
+      avg_len = ( len(entry) + avg_len * (idx) ) / (idx + 1)
+    print(f'Scanned {len(entries)} `{data["filename"]}` entries.')
+    print(f'Minimum related entries length was {min_len}')
+    print(f'Maximum related entries length was {max_len}')
+    print(f'Average related entries length was {avg_len}')
+
+
+if __name__ == "__main__":
+  correct_jsons()
+  for file in [SKILLS_JSON, JOBS_JSON]:
+    print_stats(file)
